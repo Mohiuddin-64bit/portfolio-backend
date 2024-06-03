3de3f8e1a6fb06aebbab36fb79f79a14cb180681
Mohiuddin <mohiuddin.niddu@gmail.com>
2024-06-03 15:51:33 +0600

    chore: update CORS origin URL for production deployment

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-// app.use(cors({ origin: "https://community-eats.vercel.app", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mohiuddin200.vercel.app/", credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection URL
